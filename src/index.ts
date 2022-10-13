ddf6764714e0878028504896980b274618121de6
João Lucca Oliveira Schmitz <contato.joaoluccars@gmail.com>
2022-10-13 00:35:41 -0300

    Added discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export const hello = () => {
-  return "Hello word";
-};
+import "dotenv/config";
+import { Client, GatewayIntentBits, REST } from "discord.js";
+
+const TOKEN = process.env.BOT_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const rest = new REST({ version: "10" }).setToken(TOKEN);
